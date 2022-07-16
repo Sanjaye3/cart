ae72f6bf90efb58726d53b4d13da2f6b19d62fd6
Sanjaye3 <92200293+Sanjaye3@users.noreply.github.com>
2022-07-16 15:30:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import Cart from "C:\\Users\\SanjayeJ\\Desktop\\Cart\\my-app\\src\\Components\\Cart.js";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       
+       <div className="header">
+                <h1>Shop in style</h1>
+                <h3>With this shop homepage template</h3>
+                </div>
+                <Cart />
+                <Cart />
+                <Cart />
+        <div className="footer">
+                    <p>Copyright © Your Website 2022</p>
+          </div>
+       </div>
+     
+  );
+}
+
+export default App;
---
+++ Cart.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "C:\\Users\\SanjayeJ\\Desktop\\Cart\\my-app\\src\\Components\\Cart.css";
+ 
+
+function Cart(){
+    return(
+            <div className="Cart">
+                <div className="Upper">
+                <p>450 x 300</p>
+
+                </div>
+
+
+                <div className="Lower">
+                <h3>Fancy Product</h3>
+                <p>$40.00 - $80.00</p>
+                <button>View Options</button>
+
+
+                </div>
+
+
+
+               
+            </div>
+
+
+    )
+}
+
+
+export default Cart ;
